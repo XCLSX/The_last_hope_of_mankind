8b576d8a6d852bdf812d1a25e8a2db4ff29a6e5f
blackbamboo2001 <wangzhilin2001@outlook.com>
2022-09-18 22:46:14 +0800

    随生成怪物增多而加快怪物生成

--- Assets/Resources/texture/create_monster.cs
+++ Assets/Resources/texture/create_monster.cs
@@ -7,9 +7,11 @@ public class create_monster : MonoBehaviour
     // Start is called before the first frame update
     public Transform[] create_point;
     public float timer = 1.0f;
+    public float timer_reduce = 1.0f;//作为赋给怪物生成定时器的时间长度，随击杀怪物数量增多而减少
+    int monster_num = 0;
     void Start()
     {
-
+        timer = timer_reduce;
     }
 
     // Update is called once per frame
@@ -20,8 +22,15 @@ public class create_monster : MonoBehaviour
         if (timer <= 0)
         {
             create_monsters(i);
-            timer = 1.0f;
+            monster_num++;
+            timer = timer_reduce;
+            if (monster_num % 10 == 0)
+            {
+                if (timer_reduce > 0.2f)
+                    timer_reduce -= 0.1f;
+            }
         }
+
     }
 
     void create_monsters(int i)
